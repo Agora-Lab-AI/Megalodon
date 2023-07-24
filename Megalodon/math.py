8203130b84f9e9db97ff90de168f8177da9f56d4
Zack <zack@zackbradshaw.com>
2023-07-24 17:38:07 -0500

    fix: Add missing packages

--- Megalodon/math.py
+++ Megalodon/math.py
@@ -3,13 +3,13 @@ from datasets import load_dataset
 import openai
 from datasets import load_dataset, DatasetDict, Dataset
 
-from Megalodon.models import OpenAILanguageModel, HuggingFaceLLM
+from Megaladon.models import OpenAILanguageModel, HuggingFaceLLM
 
 class Megalodon:
     def __init__(self, model_id: str, api_key: str = None, dataset: str = None, prompt: str = None):
         self.dataset = load_dataset(dataset) if dataset else None
         self.prompt = prompt if prompt else "This text: {text} was upvoted/downvoted. Explain why in a simple, comprehensive manner."
-        
+
         if "gpt" in model_id.lower():
             self.model = OpenAILanguageModel(api_key, api_model=model_id)
         else:
@@ -17,7 +17,7 @@ class Megalodon:
 
     def preprocess_sample(self, sample):
         text = ' '.join(str(sample[key]) for key in sample.keys())
-        return text 
+        return text
 
     def explain_votes(self, text):
         prompt = self.prompt.format(text=text)
@@ -41,19 +41,20 @@ class Megalodon:
 
 
 
-    
+
 
 
 # TODO: Make it modular to plug in and play with any model openai or huggingface
 # TODO: Save to huggingface after each iteration is labelled
 # TODO: Potentially use parquet for optimized storage
-# TODO: Add in polymorphic or shape shifting preprocesing logi 
-# # Using OpenAI model
-# megalodon = Megalodon(model_id="gpt-3", api_key="your-api-key", dataset="flax-sentence-embeddings/stackexchange_math_jsonl")
+# TODO: Add in polymorphic or shape shifting preprocesing logi
+
+# Using OpenAI model
+# megalodon = Megalodon(model_  id="gpt-3", api_key="your-api-key", dataset="flax-sentence-embeddings/stackexchange_math_jsonl")
 # explanations = megalodon.run()
-# megalodon.save_to_huggingface(explanations, 'hf_output_dir')
+# megalodon.save_to_huggingface(explanations, 'Wawaa/Megalodon')
 
 # # Using Hugging Face model
 # megalodon = Megalodon(model_id="gpt2", dataset="flax-sentence-embeddings/stackexchange_math_jsonl")
 # explanations = megalodon.run()
-# megalodon.save_to_huggingface(explanations, 'hf_output_dir')
+# megalodon.save_to_huggingface(explanations, 'Wawaa/Megalodon')
