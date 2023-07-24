a514a7739f6e949ea4f678ed52cd5ba878a2e62a
Kye <kye@apacmediasolutions.com>
2023-07-24 16:18:21 -0400

    clean up

--- Megalodon/math.py
+++ Megalodon/math.py
@@ -1,4 +1,8 @@
+import os
 from datasets import load_dataset
+import openai
+
+api_key = os.getenv("OPENAI_API_KEY")
 
 dataset = load_dataset("flax-sentence-embeddings/stackexchange_math_jsonl")
 
@@ -19,4 +23,20 @@ def prep_sample(sample):
     }
 
 
-    
+def preprocess_sample(sample):
+    text = ''.join(str(sample[key]) for key in sample.keys())
+    return text 
+
+
+#generate an explanation for votes
+def explain_votes(model, text):
+    prompt = f"The text: {text} was upvoted/downvoted. Explain why thoroughly"
+    response = model(prompt=prompt, max_tokens=60)
+    return response.choice[0].text.strip()
+
+
+#iterate for sample dataset:
+for sample in dataset:
+    sample_text = preprocess_sample(sample)
+    explanation = explain_votes(openai.GPT, sample_text)
+    print(f"Explanation: {explanation}")
